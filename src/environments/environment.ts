0caabd191ea90e32463abff975a69ddb807a3186
Ameya Suvarna <ameyaps@google.com>
2024-12-29 15:08:46 +0530

    Adding Google Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,18 @@
 export const environment = {
   firebase: {
+    apiKey: "AIzaSyD01LxV8SlqznSO2Fb0AebnW9W98qR5yNE",
+    authDomain: "friendlychat-21878.firebaseapp.com",
+    projectId: "friendlychat-21878",
+    storageBucket: "friendlychat-21878.firebasestorage.app",
+    messagingSenderId: "114702013635",
+    appId: "1:114702013635:web:a39046a88c308270dffe76",
+    measurementId: "G-GXFEWMNCKL"
+  },
+};
+
+/*
+export const environment = {
+  firebase: {
     apiKey: 'API_KEY',
     authDomain: 'PROJECT_ID.firebaseapp.com',
     databaseURL: 'https://PROJECT_ID.firebaseio.com',
@@ -10,3 +23,4 @@ export const environment = {
     measurementId: 'G-MEASUREMENT_ID',
   },
 };
+/*
